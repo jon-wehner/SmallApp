483c207d996dd5f88bd171e68f9e49901a6344d7
Jon Wehner <jonjwehner@gmail.com>
2021-09-16 12:13:30 -0700

    added carousel

--- index.js
+++ index.js
@@ -8,5 +8,28 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   
   const appKeys = Object.keys(appLinks);
- 
+
+  const carouselButtons = document.querySelectorAll(".carouselButton");
+  const leftButton = carouselButtons[0];
+  const rightButton = carouselButtons[1];
+  
+  let carouselPointer = 0
+  const carouselContent = document.querySelector(".carouselContent");  
+  
+  carouselContent.innerText = appKeys[carouselPointer];
+  carouselContent.setAttribute("href", appLinks[appKeys[carouselPointer]]);
+  rightButton.addEventListener("click", () => {
+    if(carouselPointer < appKeys.length - 1) {
+      carouselPointer += 1;
+      carouselContent.innerText = appKeys[carouselPointer];
+      carouselContent.setAttribute("href", appLinks[appKeys[carouselPointer]]);
+    };
+  });
+  leftButton.addEventListener("click", () => {
+    if (carouselPointer > 0) {
+      carouselPointer -= 1
+      carouselContent.innerText = appKeys[carouselPointer];
+      carouselContent.setAttribute("href", appLinks[appKeys[carouselPointer]]);
+    }
+  });
 });
