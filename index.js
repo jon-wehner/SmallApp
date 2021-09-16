fc66bcfd3ac084b6b99d5aa8e23d55b033049856
Jon Wehner <jonjwehner@gmail.com>
2021-09-16 11:49:02 -0700

    removed some js

--- index.js
+++ index.js
@@ -8,16 +8,5 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   
   const appKeys = Object.keys(appLinks);
-
-  const carouselButtons = document.querySelectorAll(".carouselButton");
-  const leftButton = carouselButtons[0];
-  const rightButton = carouselButtons[1];
-  
-  let carouselPointer = 0
-  const carouselContent = document.querySelector(".carouselContent");
-  carouselContent.innerText = appKeys[carouselPointer];
-  carouselContent.setAttribute("href", appLinks[appKeys[carouselPointer]]);
-  rightButton.addEventListener("Click", () => {
-    carouselPointer += 1
-  })
+ 
 });
