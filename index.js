170191402e64432c6543375fdd0b6120989739b3
Jon Wehner <jonjwehner@gmail.com>
2021-09-18 11:34:47 -0700

    added fonts, updated carousel elements

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const appLinks = {
-    'Small App' : "./app.html",
-    'Small Blog' : "./blog.html",
-    'Small Shop' : "./shop.html",
-    'Small Note' : "./note.html",
-    'Small Game' : "./game.html",
+    'App' : "./app.html",
+    'Blog' : "./blog.html",
+    'Shop' : "./shop.html",
+    'Note' : "./note.html",
+    'Game' : "./game.html",
   };
   
   const appKeys = Object.keys(appLinks);
