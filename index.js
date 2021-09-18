53118d470752c57b5fe1a153a6885ddad2346ef2
Jon Wehner <jonjwehner@gmail.com>
2021-09-18 11:49:40 -0700

    initial styling

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const appLinks = {
-    'App' : "./app.html",
-    'Blog' : "./blog.html",
-    'Shop' : "./shop.html",
-    'Note' : "./note.html",
-    'Game' : "./game.html",
+    'app' : "./app.html",
+    'blog' : "./blog.html",
+    'shop' : "./shop.html",
+    'note' : "./note.html",
+    'game' : "./game.html",
   };
   
   const appKeys = Object.keys(appLinks);
 
   const carouselButtons = document.querySelectorAll(".carouselButton");
   const leftButton = carouselButtons[0];
-  const rightButton = carouselButtons[1];
-  
-  let carouselPointer = 0
+  const rightButton = carouselButtons[1];  
+
   const carouselContent = document.querySelector(".carouselContent");
   
   const rotateRight = () => {
@@ -28,8 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
     carouselContent.setAttribute("href", appLinks[appKeys[0]]);
   }
   
-  carouselContent.innerText = appKeys[carouselPointer];
-  carouselContent.setAttribute("href", appLinks[appKeys[carouselPointer]]);
+  carouselContent.innerText = appKeys[0];
+  carouselContent.setAttribute("href", appLinks[appKeys[0]]);
   rightButton.addEventListener("click", () => {
     rotateRight();
   });
